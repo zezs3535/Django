c21f6ee1a70e33ae19c88b801adaa0aa47a84efd
zezs3535 <zezs3535@naver.com>
2020-09-14 22:07:57 +0900

    Django Basic

---
+++ community/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from community.forms import *
+from django.shortcuts import render
+
+def write(request):
+    if request.method=="POST":
+        form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = Form()
+    
+    return render(request, 'write.html', {'form':form})
+
+def list(request):
+    articleList = Article.objects.all()
+    return render(request, 'list.html' ,{'articleList':articleList})
+
+def view(request, num="1"):
+    article = Article.objects.get(id=num)
+    return render(request, 'view.html', {'article':article})
